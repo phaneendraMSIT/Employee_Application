eee63cb43a869f1a22be946bce63d97b2bb8f90b
Phaneendra Kasanur <phaneendra.kasanur@recykal.com>
2019-05-08 11:28:29 +0530

    added comment on dept 1.

--- src/main/java/com/springBoot/demo_Assignment/Controller/DepartmentController.java
+++ src/main/java/com/springBoot/demo_Assignment/Controller/DepartmentController.java
@@ -33,7 +33,7 @@ public class DepartmentController {
         return departmentService.findById(departmentId);
     }
 
-    //its adding dept
+    //its adding dept - updated1
     @PostMapping("/department")
     public  void addDepartment(@RequestBody Department department){
         departmentService.addDepartment(department);
